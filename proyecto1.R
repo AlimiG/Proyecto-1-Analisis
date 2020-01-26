81c2af215c84201c54262eb315b157d00ec8fcde
Alimi <alimigarmendia@gmail.com>
2020-01-25 22:40:25 -0400

    sigo aqui

--- proyecto1.R
+++ proyecto1.R
@@ -9,19 +9,32 @@ representation <- table(clase[race])
 bp <- barplot(representation,names.arg =c("Grupo A","Grupo B","Grupo C","Grupo D","Grupo E"),main = "Representacion del salon por raza etnica")
 abline(h=0)
 text(bp, representation/2,labels = round(representation*100/(sum(representation)),digits = 1))
+
+
+
 # Ahora seleccionamos solo los alumnos de la raza C
 raceC = clase[clase$race.ethnicity == "group C",]
-## Comenzaremos por analizar las notas segun el genero de los alumnos
-# Procedemos a realizar una comparacion
-par(mfrow = c(2,3))
-par(mar = c(4,4,1,1))
-boxplot((raceC[raceC$gender == "male",])[6],ylab = "Puntaje",xlab  = "Math")
-boxplot((raceC[raceC$gender == "male",])[7],ylab = "Puntaje",xlab  = "Writting")
-boxplot((raceC[raceC$gender == "male",])[8],ylab = "Puntaje",xlab  = "Reading")
-text(line2user(line = mean(par('mar')[c(2,4)],side=2),
-               line2user(line=2),side = 3),'Male',xpd=NA,cex = 2,font = 2)
-boxplot((raceC[raceC$gender == "female",])[6],ylab = "Puntaje",xlab  = "Math")
-boxplot((raceC[raceC$gender == "female",])[7],ylab = "Puntaje",xlab  = "Writting")
-boxplot((raceC[raceC$gender == "female",])[8],ylab = "Puntaje",xlab  = "Reading")
-mtext("Female",side = 2, line = 2,outer = TRUE)
+boxplot(raceC[6:8],ylab = "Puntaje", names=c("Math","Reading","Writting"), main = "Desempeno de los alumnos de raza C")
 
+
+#Revisamos la correlacion entre materias
+library(xts)
+library(zoo)
+library(PerformanceAnalytics)
+chart.Correlation(raceC[6:8],histogram = F,pch = 16)
+boxplot((raceC[raceC$gender == "male",])[6:8],ylab = "Puntaje",names = c("Math","Reading",'Writting'),main = 'Masculino')
+boxplot((raceC[raceC$gender == "female",])[6:8],ylab = "Puntaje",names = c("Math","Reading",'Writting'),main = 'Femenino')
+
+
+#Alumnos alejados del centroide
+dist = mahalanobis(raceC[6:8],colMeans(raceC[6:8]),cov(raceC[6:8]))
+hist(dist, main = "Distancia de Mahalanobis",xlab = "Distancia de Mahalanobis",ylab = "Frecuencia")
+dim(raceC[dist>1,])[1]
+
+
+## Comenzaremos por analizar las notas segun el genero de los alumnos
+# Procedemos a realizar una comparacion por genero
+genero <- table(raceC[gender])
+bp <- barplot(genero,names.arg =c("Masculino","Femenino"),main = "Representacion por genero")
+abline(h=0)
+text(bp, genero/2,labels = round(genero*100/(sum(genero)),digits = 1))
